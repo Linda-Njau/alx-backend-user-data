f687ac8deb2a7a78a311dcb4e47cbe25b6e503cf
Linda-Njau <lindanjau21@gmail.com>
2023-08-16 23:47:55 +0300

    Rectified get_reset_password_token function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,7 +77,7 @@ class Auth:
         except NoResultFound:
             raise ValueError
         reset_token = _generate_uuid()
-        self._db.update_user(user.reset_token, reset_token=reset_token)
+        self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
 
     def update_password(self, password: str, reset_token: str) -> None:
