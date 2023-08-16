a49200db00a9173c9fcff86e7a4b4f528b001a8f
Linda-Njau <lindanjau21@gmail.com>
2023-08-16 23:52:20 +0300

    Type annotation for update_password function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,7 +82,7 @@ def reset_password():
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password():
+def update_password() -> str:
     """Updates the password of the user"""
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
